54b504054ae010e3f69152a0231b68d25a79cd3a
Aniket Kulkarni <aniketkulkarni@Anikets-MBP.bbrouter>
2022-06-17 17:39:01 +0530

    Initial commits required for project

---
+++ src/test/java/com/example/JersyRestCRUD/JersyRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JersyRestCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JersyRestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
